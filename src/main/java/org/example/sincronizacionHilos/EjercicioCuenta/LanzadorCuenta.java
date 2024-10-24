6d530da3c4d3aa037e77ba5d24305d19191d9491
ANavarro96 <alejandronavarro96@hotmail.com>
2024-10-24 10:50:16 +0100

    Añadido código del ejercicio de syncro

--- src/main/java/org/example/sincronizacionHilos/EjercicioCuenta/LanzadorCuenta.java
+++ src/main/java/org/example/sincronizacionHilos/EjercicioCuenta/LanzadorCuenta.java
@@ -12,6 +12,16 @@ public class LanzadorCuenta {
         Cuenta c = new Cuenta();
         ArrayList<Thread> listaH = new ArrayList<>();
         // TODO
+        for(int i = 0; i < 100; i++) listaH.add(new Thread(c));
+        listaH.forEach(Thread::start);
+        listaH.forEach(thread -> {
+            try {
+                thread.join(); // Join the thread and wait for its completion
+            } catch (InterruptedException e) {
+                // Handle the exception, for example, by printing the stack trace or logging
+                e.printStackTrace();
+            }
+        });
 
         System.out.println(Cuenta.getCuenta());
     }
