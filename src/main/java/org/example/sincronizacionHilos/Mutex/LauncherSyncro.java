41ebd21af2e0bbe02809a5a09e125e2d7c982ae5
ANavarro96 <alejandronavarro96@hotmail.com>
2024-10-24 11:49:52 +0100

    Añadido ejemplos de semáforos y productor/consumidor

--- src/main/java/org/example/sincronizacionHilos/Mutex/LauncherSyncro.java
+++ src/main/java/org/example/sincronizacionHilos/Mutex/LauncherSyncro.java
@@ -2,28 +2,30 @@ package org.example.sincronizacionHilos.Mutex;
 
 import org.example.sincronizacionHilos.EjercicioCuenta.Cuenta;
 
+import java.lang.reflect.Array;
 import java.util.ArrayList;
+import java.util.Random;
+import java.util.Set;
 
 public class LauncherSyncro {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
 
-        // TODO: lanzar 5, 50 y 100 hilos con Cuenta y mostrar el resultado
-        Cuenta.max_veces = 0;
-        Cuenta c = new Cuenta();
-        ArrayList<Thread> listaH = new ArrayList<>();
-        // TODO
-        for(int i = 0; i < 100; i++) listaH.add(new Thread(c));
-        listaH.forEach(Thread::start);
-        listaH.forEach(thread -> {
-            try {
-                thread.join(); // Join the thread and wait for its completion
-            } catch (InterruptedException e) {
-                // Handle the exception, for example, by printing the stack trace or logging
-                e.printStackTrace();
-            }
-        });
+        // TODO: ¿Cuál es la salida?
+        ArrayList<SyncroJava> listaObjetos = new ArrayList<>();
+        ArrayList<Thread> listaHilos = new ArrayList<>();
 
-        System.out.println(Cuenta.getCuenta());
+        for (int i = 0; i < 10; i++) listaObjetos.add(new SyncroJava((int) Math.round( Math.random() )));
+
+        for(SyncroJava s : listaObjetos) listaHilos.add(new Thread(s, "Hilo num " + listaObjetos.indexOf(s)));
+
+
+        for(Thread t : listaHilos) t.start();
+
+
+
+        for(Thread t : listaHilos) t.join();
+
+
 
     }
 }
